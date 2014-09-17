bdcd33b60f3111975228cd8c8ac292880c74380b
AleksMorozova <aleks_morozova91@mail.ru>
2014-09-17 17:46:29 +0300

    change design of SetupApplicationMainForm

--- src/PrizmSetupApplication/Forms/SetupApplicationMainForm.Designer.cs
+++ src/PrizmSetupApplication/Forms/SetupApplicationMainForm.Designer.cs
@@ -58,38 +58,32 @@
             // 
             // lblDesign
             // 
-            this.lblDesign.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.lblDesign.Location = new System.Drawing.Point(32, 76);
+            this.lblDesign.Location = new System.Drawing.Point(29, 61);
             this.lblDesign.Name = "lblDesign";
-            this.lblDesign.Size = new System.Drawing.Size(48, 19);
+            this.lblDesign.Size = new System.Drawing.Size(36, 13);
             this.lblDesign.TabIndex = 7;
-            this.lblDesign.Text = "Design";
+            this.lblDesign.Text = "Design:";
             // 
             // lblClient
             // 
-            this.lblClient.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.lblClient.Location = new System.Drawing.Point(32, 23);
+            this.lblClient.Location = new System.Drawing.Point(29, 23);
             this.lblClient.Name = "lblClient";
-            this.lblClient.Size = new System.Drawing.Size(40, 19);
+            this.lblClient.Size = new System.Drawing.Size(31, 13);
             this.lblClient.TabIndex = 6;
-            this.lblClient.Text = "Client";
+            this.lblClient.Text = "Client:";
             // 
             // txtDesign
             // 
-            this.txtDesign.Location = new System.Drawing.Point(138, 20);
+            this.txtDesign.Location = new System.Drawing.Point(117, 20);
             this.txtDesign.Name = "txtDesign";
-            this.txtDesign.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.txtDesign.Properties.Appearance.Options.UseFont = true;
-            this.txtDesign.Size = new System.Drawing.Size(251, 26);
+            this.txtDesign.Size = new System.Drawing.Size(251, 20);
             this.txtDesign.TabIndex = 5;
             // 
             // txtClient
             // 
-            this.txtClient.Location = new System.Drawing.Point(138, 69);
+            this.txtClient.Location = new System.Drawing.Point(117, 58);
             this.txtClient.Name = "txtClient";
-            this.txtClient.Properties.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
-            this.txtClient.Properties.Appearance.Options.UseFont = true;
-            this.txtClient.Size = new System.Drawing.Size(251, 26);
+            this.txtClient.Size = new System.Drawing.Size(251, 20);
             this.txtClient.TabIndex = 4;
             // 
             // splitContainer
@@ -98,7 +92,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.splitContainer.FixedPanel = DevExpress.XtraEditors.SplitFixedPanel.None;
-            this.splitContainer.Location = new System.Drawing.Point(29, 109);
+            this.splitContainer.Location = new System.Drawing.Point(29, 99);
             this.splitContainer.Name = "splitContainer";
             this.splitContainer.Panel1.Controls.Add(this.gridControlPlate);
             this.splitContainer.Panel1.Controls.Add(this.lblPlate);
@@ -106,8 +100,8 @@
             this.splitContainer.Panel2.Controls.Add(this.gridControlPipe);
             this.splitContainer.Panel2.Controls.Add(this.lblPipe);
             this.splitContainer.Panel2.Text = "PipePanel";
-            this.splitContainer.Size = new System.Drawing.Size(632, 306);
-            this.splitContainer.SplitterPosition = 316;
+            this.splitContainer.Size = new System.Drawing.Size(627, 300);
+            this.splitContainer.SplitterPosition = 314;
             this.splitContainer.TabIndex = 8;
             this.splitContainer.Text = "splitContainerControl1";
             // 
@@ -119,7 +113,7 @@
             this.gridControlPlate.Location = new System.Drawing.Point(3, 57);
             this.gridControlPlate.MainView = this.gridViewPlate;
             this.gridControlPlate.Name = "gridControlPlate";
-            this.gridControlPlate.Size = new System.Drawing.Size(302, 241);
+            this.gridControlPlate.Size = new System.Drawing.Size(300, 235);
             this.gridControlPlate.TabIndex = 10;
             this.gridControlPlate.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridViewPlate});
@@ -140,10 +134,9 @@
             // 
             // lblPlate
             // 
-            this.lblPlate.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
             this.lblPlate.Location = new System.Drawing.Point(3, 20);
             this.lblPlate.Name = "lblPlate";
-            this.lblPlate.Size = new System.Drawing.Size(139, 19);
+            this.lblPlate.Size = new System.Drawing.Size(97, 13);
             this.lblPlate.TabIndex = 9;
             this.lblPlate.Text = "Plate manufacturers";
             // 
@@ -157,7 +150,7 @@
             this.gridControlPipe.Name = "gridControlPipe";
             this.gridControlPipe.RepositoryItems.AddRange(new DevExpress.XtraEditors.Repository.RepositoryItem[] {
             this.SizeGradeItemCheckedBox});
-            this.gridControlPipe.Size = new System.Drawing.Size(302, 241);
+            this.gridControlPipe.Size = new System.Drawing.Size(299, 235);
             this.gridControlPipe.TabIndex = 11;
             this.gridControlPipe.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
             this.gridViewPipe});
@@ -194,40 +187,36 @@
             // 
             // lblPipe
             // 
-            this.lblPipe.Appearance.Font = new System.Drawing.Font("Tahoma", 12F);
+            this.lblPipe.Cursor = System.Windows.Forms.Cursors.No;
             this.lblPipe.Location = new System.Drawing.Point(14, 20);
             this.lblPipe.Name = "lblPipe";
-            this.lblPipe.Size = new System.Drawing.Size(135, 19);
+            this.lblPipe.Size = new System.Drawing.Size(93, 13);
             this.lblPipe.TabIndex = 10;
             this.lblPipe.Text = "Pipe manufacturers";
             // 
             // btnCancel
             // 
             this.btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCancel.Appearance.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.btnCancel.Appearance.Options.UseFont = true;
-            this.btnCancel.Location = new System.Drawing.Point(586, 431);
+            this.btnCancel.Location = new System.Drawing.Point(580, 416);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(75, 23);
             this.btnCancel.TabIndex = 11;
-            this.btnCancel.Text = "Cancel";
+            this.btnCancel.Text = "Close";
             // 
             // btnOK
             // 
             this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnOK.Appearance.Font = new System.Drawing.Font("Tahoma", 10F);
-            this.btnOK.Appearance.Options.UseFont = true;
-            this.btnOK.Location = new System.Drawing.Point(475, 431);
+            this.btnOK.Location = new System.Drawing.Point(469, 416);
             this.btnOK.Name = "btnOK";
             this.btnOK.Size = new System.Drawing.Size(75, 23);
             this.btnOK.TabIndex = 10;
-            this.btnOK.Text = "OK";
+            this.btnOK.Text = "Export";
             // 
             // SetupApplicationMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(689, 466);
+            this.ClientSize = new System.Drawing.Size(684, 460);
             this.Controls.Add(this.btnCancel);
             this.Controls.Add(this.btnOK);
             this.Controls.Add(this.splitContainer);
