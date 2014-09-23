896852f21aca80a6d1c8971e539b4c42c7a8f536
demmy <demmy.uber@gmail.com>
2014-09-23 10:38:19 +0300

    add dummy data class for mock data

---
+++ src/PrizmMainProject/Dev/DummyData.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrizmMain.Dev
+{
+    public class DummyData
+    {
+        public static DataTable CreateDataTable()
+        {
+            //table
+            DataTable table = new DataTable("Data");
+
+            //columns
+            table.Columns.Add("Id", typeof (Int32));
+            table.Columns.Add("Stage", typeof (String));
+            table.Columns.Add("Object", typeof (String));
+            table.Columns.Add("Frequency", typeof (String));
+            table.Columns.Add("Document", typeof (String));
+            table.Columns.Add("Sertification", typeof (String));
+            table.Columns.Add("InspectionPlant", typeof (String));
+            table.Columns.Add("InspectionCustomer", typeof (String));
+            table.Columns.Add("InspectionOther", typeof (String));
+
+            table.Rows.Add(new object[]
+            {
+                1, "Сварка внутренних швов", "Параметры сварки", "Каждая труба",
+                "MPS-AGP(c X-BO-E-PL-SIP-0007-000-0 M WPS DUMMY DATA)", "R,C", "WP", "", "R,M R,M R,M R,M R,M R,M R,M R,M R,M "
+            });
+            table.Rows.Add(new object[]
+            {
+                2, "Сварка внутренних швов", "Параметры сварки", "Каждая труба",
+                "MPS-AGP(c X-BO-E-PL-SIP-0007-000-0 M WPS DUMMY DATA)", "R", "X,WP", "X", "R,M"
+            });
+            table.Rows.Add(new object[]
+            {
+                3, "Сварка внутренних швов", "Параметры сварки", "Каждая труба",
+                "MPS-AGP(c X-BO-E-PL-SIP-0007-000-0 M WPS DUMMY DATA)", "R", "WP", "", "R,M"
+            });
+            table.Rows.Add(new object[]
+            {
+                4, "Сварка внутренних швов", "Параметры сварки", "Каждая труба",
+                "MPS-AGP(c X-BO-E-PL-SIP-0007-000-0 M WPS DUMMY DATA)", "R,C", "X,WP", "", "R,M"
+            });
+            table.Rows.Add(new object[]
+            {
+                5, "Сварка внутренних швов", "Параметры сварки", "Каждая труба",
+                "MPS-AGP(c X-BO-E-PL-SIP-0007-000-0 M WPS DUMMY DATA)", "R,C", "X", "WP", "R,M"
+            });
+
+            return table;
+        }
+    }
+}
