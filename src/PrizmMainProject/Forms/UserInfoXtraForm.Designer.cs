fa01d09d5e89c09506ca17262cb46929483840b6
AleksMorozova <aleks_morozova91@mail.ru>
2014-09-24 12:28:57 +0300

    Update Joint and UserInf Forms

--- src/PrizmMainProject/Forms/UserInfoXtraForm.Designer.cs
+++ src/PrizmMainProject/Forms/UserInfoXtraForm.Designer.cs
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // UserInfoXtraForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 425);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "UserInfoXtraForm";
             this.Text = "UserInfoXtraForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
