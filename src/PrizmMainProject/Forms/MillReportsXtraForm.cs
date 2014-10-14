e01e3e5fa214eb2f593c7dab7d24a66c9accdd6a
olga-jane <olga.jane.vakhromova@gmail.com>
2014-10-14 16:52:36 +0300

    reports new empty forms

---
+++ src/PrizmMainProject/Forms/MillReportsXtraForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace PrizmMain.Forms
+{
+    public partial class MillReportsXtraForm : DevExpress.XtraEditors.XtraForm
+    {
+        public MillReportsXtraForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
