bbdf8b5e19fbb335841e0de8225e471044ed3317
demmy <demiurge.ds@gmail.com>
2014-10-21 16:10:18 +0300

    Domain, Data for user/role

---
+++ src/Domain/DAL/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DAL
+{
+    public interface IUserRepository : IRepository
+    {
+        //User Specific Operation
+    }
+}
