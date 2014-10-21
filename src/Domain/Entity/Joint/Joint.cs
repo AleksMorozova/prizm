d904f5604f27ec85dac2379a4e745c443a39f3bd
demmy <demiurge.ds@gmail.com>
2014-10-21 17:34:57 +0300

    Joint entitis

---
+++ src/Domain/Entity/Joint/Joint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.Joint
+{
+    public class Joint : PipelinePiece
+    {
+        public Joint()
+        {
+            this.Weldings = new List<ConstructionWelding>();
+            this.Inspections = new List<JointInspection>();
+        }
+
+        public Guid JointID { get; set; }
+        public string Number { get; set; }
+        public DateTime LoweringDate { get; set; }
+        public float DistansFromKilomiterPost { get; set; }
+
+        public ICollection<ConstructionWelding> Weldings { get; set; }
+        public ICollection<JointInspection> Inspections { get; set; }
+        
+    }
+}
