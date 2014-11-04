e16df4c75ab42a23f568032d00094fbc69cc8581
demmy <demiurge.ds@gmail.com>
2014-11-04 18:21:11 +0200

    Update entity for Mill

---
+++ src/Domain/Entity/Mill/Heat.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.Mill
+{
+    public class Heat
+    {
+        public virtual int Id { get; set; }
+        public virtual string Number { get; set; }
+        public virtual PhysicalParameters PhysicalParameters { get; set; }
+        public virtual ChemicalComposition ChemicalComposition { get; set; }
+    }
+}
---
+++ src/Domain/Entity/Mill/PipeTestResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.Mill
+{
+    public class PipeTestResult
+    {
+        public virtual Guid Id { get; set; }
+        public virtual PipeTest operation { get; set; }
+        public virtual DateTime Date { get; set; }
+    }
+}
