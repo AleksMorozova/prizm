6105c8a6f63c1ca8390abbd120355a45636bde4e
DmitrySidorov <sidorov@email.ua>
2014-11-12 16:45:04 +0200

    update mapping for RailcarMap.cs and PurchaseOrderMap.cs

--- src/Data/DAL/Mapping/RailcarMap.cs
+++ src/Data/DAL/Mapping/RailcarMap.cs
@@ -3,16 +3,14 @@ using FluentNHibernate.Mapping;
 
 namespace Data.DAL.Mapping
 {
-    public class RailcarMap : ClassMap<Railcar>
+    public class RailcarMap : SubclassMap<Railcar>
     {
         public RailcarMap()
         {
-            Id(_ => _.Id, "id").Column("id").GeneratedBy.GuidComb();
-            Map(_ => _.Number, "number");
+            Map(x => x.Number).Column("number");
             Map(_ => _.ShippingDate, "shipingDate");
             Map(_ => _.Destination, "destination");
             Map(_ => _.Certificate, "certificate");
-            Map(_ => _.IsActive, "is_active");
         }
     }
 }
