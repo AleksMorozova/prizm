4d1139a85b1ea3b3a967fed45c529ce9991bf0ff
demmy <demiurge.ds@gmail.com>
2014-11-14 16:28:38 +0200

    add collection

--- src/Domain/Entity/Mill/Railcar.cs
+++ src/Domain/Entity/Mill/Railcar.cs
@@ -1,14 +1,22 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Domain.Entity.Mill
 {
     public class Railcar : Item
     {
+        public Railcar()
+        {
+            this.Pipes = new List<Pipe>();
+        }
+
         public virtual string Number { get; set; }
         public virtual string Certificate { get; set; }
         public virtual string Destination { get; set; }
         public virtual DateTime ShippingDate { get; set; }
         public virtual DateTime DeliveryDate { get; set; }
+
+        public virtual IList<Pipe> Pipes { get; set; }
 
         //TODO: back reference to pipes
     }
