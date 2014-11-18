fabd0465e2d1dba077a556aa140a5e239b824a5a
kapcia <kapcentrino@gmail.com>
2014-11-18 08:34:54 +0200

    Project settings

---
+++ src/Data/DAL/Hibernate/ProjectRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entity;
+using Ninject;
+using NHibernate;
+
+namespace Data.DAL.Hibernate
+{
+    public class ProjectRepository : AbstractHibernateRepository<Guid, Project>, IProjectRepository
+    {
+         [Inject]
+        public ProjectRepository(ISession session)
+            : base(session)
+        {
+        }
+    }
+}
--- src/Domain/Entity/Project.cs
+++ src/Domain/Entity/Project.cs
@@ -3,8 +3,9 @@
     // FIX: remove??
     public class Project : Item
     {
-        public virtual string Title { get; set; }
+        //public virtual string Title { get; set; }
         public virtual PersonName Client { get; set; }
         public virtual PersonName Designer { get; set; }
+        public virtual int DocumentSizeLimit { get; set; }
     }
 }
