3c7b2f23a79cf73bb90c6fee7f549270f6b24235
demmy <demiurge.ds@gmail.com>
2014-11-21 13:57:20 +0200

    UnshipRailcarCommandTest v2

--- src/UnitTests/Forms/Railcar/Edit/UnshipRailcarCommandTest.cs
+++ src/UnitTests/Forms/Railcar/Edit/UnshipRailcarCommandTest.cs
@@ -9,51 +9,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace UnitTests.Forms.Railcar.Edit
 {
     [TestFixture]
     public class UnshipRailcarCommandTest
     {
-        Domain.Entity.Mill.Railcar railcar;
         Mock<IRailcarRepositories> repos;
         Mock<IPipeRepository> pipeRepo;
         Mock<IRailcarRepository> carRepo;
         RailcarViewModel viewModel;
         UnshipRailcarCommand command;
 
-        [SetUp]
-        public void Init()
+        [Test]
+        public void UnshipRailcarCommand()
         {
-            railcar = new Domain.Entity.Mill.Railcar()
-            {
-                Number = "Test Railcar",
-
-
-            };
+            repos = new Mock<IRailcarRepositories>();
+            pipeRepo = new Mock<IPipeRepository>();
+            carRepo = new Mock<IRailcarRepository>();
 
             pipeRepo.Setup(x => x.GetStored()).Returns(new List<Pipe>() { new Pipe() });
             repos.SetupGet(_ => _.PipeRepo).Returns(pipeRepo.Object);
             repos.SetupGet(_ => _.RailcarRepo).Returns(carRepo.Object);
 
-            var viewModel = new RailcarViewModel(repos.Object, "");
-            viewModel.Railcar.Number = "Test Railcar";
-            viewModel.Railcar.Pipes.Add(new Pipe());
-            var command = new SaveRailcarCommand(viewModel, repos.Object);
-        }
 
+            viewModel = new RailcarViewModel(repos.Object, "");
+            viewModel.Railcar.Number = "Railcar";
+            command = new UnshipRailcarCommand(viewModel, repos.Object);
 
-        [Test]
-        public void UnshipCommandShouldResetShippingDate()
-        {
-          
-        }
+            command.Execute();
 
-        [Test]
-        public void UnshipCommandShouldChangePipeStatus()
-        {
+            Assert.That(viewModel.Railcar.ShippingDate, Is.EqualTo(DateTime.MinValue));
 
         }
-
     }
 }
