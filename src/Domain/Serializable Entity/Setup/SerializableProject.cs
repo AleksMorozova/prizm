726b15df4a4d588438e3a34d44145a46ba40ea50
AleksMorozova <aleks_morozova91@mail.ru>
2014-11-21 16:11:08 +0200

    Create serializable entities for Mill

---
+++ src/Domain/Serializable Entity/Setup/SerializableProject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Serializable_Entity.Setup
+{
+    [Serializable]
+    class SerializableProject
+    {
+        public string MillName { get; set; }
+    }
+}
