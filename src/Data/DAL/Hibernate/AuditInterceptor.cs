078b1db0b14b52f4d11d683b0cf3aa60dc1dcb4f
kapcia <kapcentrino@gmail.com>
2014-11-28 19:19:00 +0200

    save id for object properties, log all fields while inserting

--- src/Data/DAL/Hibernate/AuditInterceptor.cs
+++ src/Data/DAL/Hibernate/AuditInterceptor.cs
@@ -15,18 +15,24 @@ namespace Data.DAL.Hibernate
         private readonly IAuditLogRepository repo;
 
         [Inject]
-        public AuditInterceptor(/*IAuditLogRepository repo*/)
+        public AuditInterceptor()
         {
-          // this.repo = repo;
             repo = new AuditLogRepository(HibernateUtil.OpenAuditSession());
         }
         enum Actions { Insert, Delete };
+
+        /// <summary>
+        /// Is called while inserting new record
+        /// </summary>
         public override bool OnSave(object entity, object id, object[] state, string[] propertyNames, NHibernate.Type.IType[] types)
         {
             LogAudit(entity, propertyNames, Actions.Insert, state);
             return base.OnSave(entity, id, state, propertyNames, types);
         }
 
+        /// <summary>
+        /// Is called while updating existing record
+        /// </summary>
         public override bool OnFlushDirty(object entity, object id, object[] currentState, object[] previousState, string[] propertyNames, NHibernate.Type.IType[] types)
         {
             if (previousState == null) return false;
@@ -35,27 +41,32 @@ namespace Data.DAL.Hibernate
             {
                 for (var i = 0; i < currentState.Count(); i++)
                 {
-                    if (currentState[i] == null && previousState[i] == null) continue;
-                    else if (currentState[i] == null || previousState[i] == null)
+                    if (currentState[i] == null && previousState[i] == null) continue; // prevent exeption 
+                    else if (currentState[i] == null || previousState[i] == null) //prevent exeption
                     {
                         string newValue = (currentState[i] == null) ? "" : currentState[i].ToString();
                         string oldValue = (previousState[i] == null || previousState == null) ? "" : previousState[i].ToString();
-                        NewAuditRecord(curentity, propertyNames[i],newValue,oldValue);
-                        
+                        NewAuditRecord(curentity, propertyNames[i], newValue, oldValue);
+
                     }
-                    else if (currentState[i].ToString() != previousState[i].ToString())
+                    else if (currentState[i] as Item == null && currentState[i].ToString() != previousState[i].ToString())
                     {
                         NewAuditRecord(curentity, propertyNames[i], currentState[i].ToString(), previousState[i].ToString());
                     }
                     else
                     {
                         var objectProperty = currentState[i] as Item;
+                        if (objectProperty!=null && currentState[i].Equals(previousState[i]) == false)
+                        { NewAuditRecord(curentity, propertyNames[i], objectProperty.Id.ToString(), objectProperty.Id.ToString()); }
                     }
                 }
             }
          return base.OnFlushDirty(entity, id, currentState, previousState, propertyNames, types);                 
         }
 
+        /// <summary>
+        /// Creating log record and saving it to DB
+        /// </summary>
         private void NewAuditRecord(Item curentity, string fieldName, string newValue, string oldValue )
         {
             AuditLog record = new AuditLog()
@@ -74,28 +85,40 @@ namespace Data.DAL.Hibernate
             repo.Commit();   
         }
 
+        /// <summary>
+        /// Is called whilr deleting record
+        /// </summary>
         public override void OnDelete(object entity, object id, object[] state, string[] propertyNames, NHibernate.Type.IType[] types)
         {
             LogAudit(entity, propertyNames, Actions.Delete, state);
             base.OnDelete(entity, id, state, propertyNames, types);
         }
 
+        /// <summary>
+        /// Log  insert/delete record (just one state)
+        /// </summary>
         private  void LogAudit(object entity, string[] propertyNames, Actions actionType, params object[] state)
         {
-            string newValue = ""; 
+            string oldValue = "just inserted", newValue = "";
             var curentity = entity as Item;
-             if (curentity != null)
-             {
-                 switch (actionType)
-                 {
-                     case Actions.Insert: newValue = "inserted";
-                         break;
-                     case Actions.Delete: newValue = "deleted";
-                         break;
-                     default: break;
-                 }
-                 NewAuditRecord(curentity, "All", newValue,"");
-             }
+            if (curentity != null)
+            {
+                for (var i = 0; i < propertyNames.Count(); i++)
+                {
+                    if (state[i] == null) continue;
+                    var objectProperty = state[i] as Item;
+                    switch (actionType)
+                    {
+                        case Actions.Insert: newValue = (objectProperty ==null)? state[i].ToString(): objectProperty.Id.ToString();
+                            break;
+                        case Actions.Delete: oldValue = (objectProperty == null) ? state[i].ToString() : objectProperty.Id.ToString();
+                            newValue = "deleted";
+                            break;
+                        default: break;
+                    }
+                    NewAuditRecord(curentity, propertyNames[i], newValue, oldValue);
+                }
+            }
         }
     }
 }
