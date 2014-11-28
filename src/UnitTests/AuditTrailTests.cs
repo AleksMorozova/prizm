51ba6986c7dbee3bb2fbf621146b5368c44ea057
kapcia <kapcentrino@gmail.com>
2014-11-28 17:05:58 +0200

    draft of unit test

--- src/UnitTests/AuditTrailTests.cs
+++ src/UnitTests/AuditTrailTests.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
 using Moq;
+using Data.DAL;
+using Data.DAL.Mill;
+using Domain.Entity;
+using Domain.Entity.Mill;
 
 namespace UnitTests
 {
@@ -12,9 +16,23 @@ namespace UnitTests
     public class AuditTrailTests
     {
         [Test]
-        public void OnSaveInterceptorTest() 
+        public void InterceptorTest() 
         {
-
+            var auditRepo = new Mock<IAuditLogRepository>();
+            var pipeRepo = new Mock<IPipeRepository>();
+            Pipe testPipe = new Pipe();
+            testPipe = new Pipe();
+            pipeRepo.Setup(_ => _.Save(testPipe));
+            auditRepo.Verify(_ => _.BeginTransaction(), Times.Once);
+            auditRepo.Verify(_ => _.Save(It.IsAny<AuditLog>()), Times.Once);
+            auditRepo.Verify(_ => _.Commit(), Times.Once());
+            auditRepo.Verify(_ => _.Evict(It.IsAny<AuditLog>()), Times.Once());
+            testPipe.Number = "test";
+            pipeRepo.Setup(_ => _.SaveOrUpdate(testPipe));
+            auditRepo.Verify(_ => _.BeginTransaction(), Times.Once);
+            auditRepo.Verify(_ => _.Save(It.IsAny<AuditLog>()), Times.Once);
+            auditRepo.Verify(_ => _.Commit(), Times.Once());
+            auditRepo.Verify(_ => _.Evict(It.IsAny<AuditLog>()), Times.Once());
         }
 
     }
