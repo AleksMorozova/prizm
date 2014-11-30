a2a0bd540cc21689f1ffa9f5d80237a925584c0a
demmy <demiurge.ds@gmail.com>
2014-11-30 16:45:06 +0200

    LengthLimit

---
+++ src/PrizmMainProject/Common/LengthLimit.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrizmMain.Common
+{
+    public class LengthLimit
+    {
+        public const int MaxHeanNumber = 20;
+        public const int MaxSteelGrade = 20;
+
+        public const int MaxInspectorFirstName = 30;
+        public const int MaxInspectorLastName = 30;
+        public const int MaxInspectorMiddleName = 30;
+        public const int MaxInspectorCertificate = 30;
+
+        public const int MaxPipeMill = 250;
+        public const int MaxPipeMillStatus = 20;
+        public const int MaxPipeNumber = 20;
+        public const int MaxPipeInspectionStatus = 15;
+        public const int MaxPipeConstructionStatus = 15;
+
+        public const int MaxPipeMillSizeType = 50;
+
+        public const int MaxPipeTestCode = 20;
+        public const int MaxPipeTestName = 50;
+        public const int MaxPipeTestSubject = 300;
+        public const int MaxPipeTestControlType = 15;
+        public const int MaxPipetestResultType = 10;
+
+        public const int MaxPipeTestResultStatus = 25;
+        public const int MaxPipeTestResultValue = 20;
+
+        public const int MaxPlateNumber = 20;
+
+        public const int MaxPurchaseOrderNumber = 20;
+
+        public const int MaxPlateManufacturerName = 50;
+        
+        public const int MaxProjectClient = 100;
+        public const int MaxProjectDesigner = 100;
+        
+        public const int MaxRailcarNumber = 20;
+        public const int MaxRailcarCertificate = 20;
+        public const int MaxRailcarDestination = 50;
+        
+        public const int MaxTestResultValue = 25;
+        public const int MaxTestResultStatus = 25;
+        
+        public const int MaxWelderFirstName = 30;
+        public const int MaxWelderLastName = 30;
+        public const int MaxWelderMiddleName = 30;
+        public const int MaxWelderCertificate = 30;
+        public const int MaxWelderStamp = 50;
+        
+        public const int MaxCoatType = 20;
+        
+        public const int MaxAuditlogUser = 50;
+        public const int MaxAuditlogTableName = 200;
+        public const int MaxAuditlogFieldName = 50;
+        public const int MaxAuditlogOldvalue = 100;
+        public const int MaxAuditlogNewValue = 100;
+    }
+}
