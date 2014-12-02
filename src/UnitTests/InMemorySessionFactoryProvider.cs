8e05698fc05b2025c064c60e46b97eb9a2efaed6
demmy <demiurge.ds@gmail.com>
2014-12-02 18:57:20 +0200

    simple test

---
+++ src/UnitTests/InMemorySessionFactoryProvider.cs
@@ -0,0 +1,61 @@
+﻿using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTests
+{
+    public class InMemorySessionFactoryProvider
+    {
+        private static InMemorySessionFactoryProvider instance;
+        public static InMemorySessionFactoryProvider Instance
+        {
+            get { return instance ?? (instance = new InMemorySessionFactoryProvider()); }
+        }
+ 
+        private ISessionFactory sessionFactory;
+        private Configuration configuration;
+ 
+        private InMemorySessionFactoryProvider() { }
+ 
+        public void Initialize()
+        {
+            sessionFactory = CreateSessionFactory();
+        }
+ 
+        private ISessionFactory CreateSessionFactory()
+        {
+            return Fluently.Configure()
+                    .Database(SQLiteConfiguration.Standard.InMemory().ShowSql())
+                    .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Data.DAL.Hibernate.HibernateUtil>())
+                    .ExposeConfiguration(cfg => configuration = cfg)
+                    .BuildSessionFactory();
+        }
+ 
+        public ISession OpenSession()
+        {
+            ISession session = sessionFactory.OpenSession();
+ 
+            var export = new SchemaExport(configuration);
+            export.Execute(true, true, false, session.Connection, null);
+ 
+            return session;
+        }
+ 
+        public void Dispose()
+        {
+            if(sessionFactory != null)
+                sessionFactory.Dispose();
+ 
+            sessionFactory = null;
+            configuration = null;
+        }
+    }
+    
+}
---
+++ src/UnitTests/Maping/PurchaseOrderMapTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Configuration;
+using FluentNHibernate.Testing;
+using Domain.Entity.Mill;
+using Data.DAL.Hibernate;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+
+namespace UnitTests.Maping
+{
+    [TestFixture]
+    class PurchaseOrderMapTest
+    {
+        [Test]
+        public void CanCorrectlyMapPurchaseOrder()
+        {
+            DateTime mydate = new DateTime(2008, 5, 1, 8, 30, 52);
+
+            InMemorySessionFactoryProvider.Instance.Initialize();
+            var session = InMemorySessionFactoryProvider.Instance.OpenSession();
+
+            new PersistenceSpecification<PurchaseOrder>(session)
+            .CheckProperty(p => p.Number, "number")
+            .CheckProperty(p => p.Date, mydate)
+            .VerifyTheMappings();
+
+        }
+
+    }
+}
