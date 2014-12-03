b3291afa0f00fd8572e39b4f08b9364179590af9
AleksMorozova <aleks_morozova91@mail.ru>
2014-12-03 13:42:39 +0200

    add column for Category in inspection grid

--- src/PrizmMainProject/Forms/PipeMill/NewEdit/MillPipeNewEditXtraForm.Designer.cs
+++ src/PrizmMainProject/Forms/PipeMill/NewEdit/MillPipeNewEditXtraForm.Designer.cs
@@ -115,6 +115,7 @@
             this.controlDateGridColumn = new DevExpress.XtraGrid.Columns.GridColumn();
             this.inspectionCodeGridColumn = new DevExpress.XtraGrid.Columns.GridColumn();
             this.inspectionCodeLookUpEdit = new DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit();
+            this.categoryGridColumn = new DevExpress.XtraGrid.Columns.GridColumn();
             this.inspectionsLayoutGroup = new DevExpress.XtraLayout.LayoutControlGroup();
             this.inspectionsLayout = new DevExpress.XtraLayout.LayoutControlItem();
             this.pipeSize = new DevExpress.XtraEditors.ComboBoxEdit();
@@ -1059,7 +1060,8 @@
             this.inspectionResultGridColumn,
             this.inspectorsGridColumn,
             this.controlDateGridColumn,
-            this.inspectionCodeGridColumn});
+            this.inspectionCodeGridColumn,
+            this.categoryGridColumn});
             this.inspectionsGridView.GridControl = this.inspections;
             this.inspectionsGridView.Name = "inspectionsGridView";
             this.inspectionsGridView.OptionsBehavior.AllowAddRows = DevExpress.Utils.DefaultBoolean.True;
@@ -1077,7 +1079,7 @@
             this.inspectionNameGridColumn.OptionsColumn.AllowEdit = false;
             this.inspectionNameGridColumn.Visible = true;
             this.inspectionNameGridColumn.VisibleIndex = 1;
-            this.inspectionNameGridColumn.Width = 274;
+            this.inspectionNameGridColumn.Width = 260;
             // 
             // expectedResultGridColumn
             // 
@@ -1087,8 +1089,8 @@
             this.expectedResultGridColumn.OptionsColumn.AllowEdit = false;
             this.expectedResultGridColumn.UnboundType = DevExpress.Data.UnboundColumnType.String;
             this.expectedResultGridColumn.Visible = true;
-            this.expectedResultGridColumn.VisibleIndex = 2;
-            this.expectedResultGridColumn.Width = 141;
+            this.expectedResultGridColumn.VisibleIndex = 3;
+            this.expectedResultGridColumn.Width = 140;
             // 
             // valueGridColumn
             // 
@@ -1097,8 +1099,8 @@
             this.valueGridColumn.FieldName = "Value";
             this.valueGridColumn.Name = "valueGridColumn";
             this.valueGridColumn.Visible = true;
-            this.valueGridColumn.VisibleIndex = 3;
-            this.valueGridColumn.Width = 135;
+            this.valueGridColumn.VisibleIndex = 4;
+            this.valueGridColumn.Width = 140;
             // 
             // testResultValue
             // 
@@ -1112,8 +1114,8 @@
             this.inspectionResultGridColumn.FieldName = "Status";
             this.inspectionResultGridColumn.Name = "inspectionResultGridColumn";
             this.inspectionResultGridColumn.Visible = true;
-            this.inspectionResultGridColumn.VisibleIndex = 4;
-            this.inspectionResultGridColumn.Width = 150;
+            this.inspectionResultGridColumn.VisibleIndex = 5;
+            this.inspectionResultGridColumn.Width = 120;
             // 
             // ResultStatusLookUpEdit
             // 
@@ -1134,8 +1136,8 @@
             this.inspectorsGridColumn.FieldName = "Inspectors";
             this.inspectorsGridColumn.Name = "inspectorsGridColumn";
             this.inspectorsGridColumn.Visible = true;
-            this.inspectorsGridColumn.VisibleIndex = 5;
-            this.inspectorsGridColumn.Width = 272;
+            this.inspectorsGridColumn.VisibleIndex = 6;
+            this.inspectorsGridColumn.Width = 211;
             // 
             // inspectorsPopupContainerEdit
             // 
@@ -1153,8 +1155,8 @@
             this.controlDateGridColumn.FieldName = "Date";
             this.controlDateGridColumn.Name = "controlDateGridColumn";
             this.controlDateGridColumn.Visible = true;
-            this.controlDateGridColumn.VisibleIndex = 6;
-            this.controlDateGridColumn.Width = 148;
+            this.controlDateGridColumn.VisibleIndex = 7;
+            this.controlDateGridColumn.Width = 131;
             // 
             // inspectionCodeGridColumn
             // 
@@ -1164,7 +1166,7 @@
             this.inspectionCodeGridColumn.Name = "inspectionCodeGridColumn";
             this.inspectionCodeGridColumn.Visible = true;
             this.inspectionCodeGridColumn.VisibleIndex = 0;
-            this.inspectionCodeGridColumn.Width = 62;
+            this.inspectionCodeGridColumn.Width = 60;
             // 
             // inspectionCodeLookUpEdit
             // 
@@ -1181,6 +1183,16 @@
             this.inspectionCodeLookUpEdit.NullText = "";
             this.inspectionCodeLookUpEdit.ValueMember = "Code";
             this.inspectionCodeLookUpEdit.EditValueChanged += new System.EventHandler(this.inspectionCodeLookUpEdit_EditValueChanged);
+            // 
+            // categoryGridColumn
+            // 
+            this.categoryGridColumn.Caption = "Категория";
+            this.categoryGridColumn.FieldName = "Operation.Category";
+            this.categoryGridColumn.Name = "categoryGridColumn";
+            this.categoryGridColumn.OptionsColumn.AllowEdit = false;
+            this.categoryGridColumn.Visible = true;
+            this.categoryGridColumn.VisibleIndex = 2;
+            this.categoryGridColumn.Width = 120;
             // 
             // inspectionsLayoutGroup
             // 
@@ -1877,6 +1889,7 @@
         private DevExpress.XtraEditors.TextEdit purchaseOrderDate;
         private DevExpress.XtraLayout.LayoutControlItem purchaseOrderDateLayoutControl;
         private DevExpress.XtraEditors.Repository.RepositoryItemLookUpEdit inspectionCodeLookUpEdit;
+        private DevExpress.XtraGrid.Columns.GridColumn categoryGridColumn;
 
     }
 }
