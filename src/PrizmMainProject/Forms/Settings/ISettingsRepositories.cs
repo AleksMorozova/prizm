1f92245561a758cb11d94ce7b439b75adcf67b6a
demmy <demiurge.ds@gmail.com>
2014-12-08 12:40:58 +0200

    Revert "Revert "Security setups""
    This reverts commit d6d03ad59196d7be1f7530e7c010f20cfdd82d12.
    
    Conflicts:
    	src/PrizmMainProject/Forms/Settings/SettingsXtraForm.Designer.cs
    	src/PrizmMainProject/Forms/Settings/SettingsXtraForm.cs

--- src/PrizmMainProject/Forms/Settings/ISettingsRepositories.cs
+++ src/PrizmMainProject/Forms/Settings/ISettingsRepositories.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Data.DAL.Security;
 
 namespace PrizmMain.Forms.Settings
 {
@@ -17,6 +18,9 @@ namespace PrizmMain.Forms.Settings
       IInspectorRepository InspectorRepo { get; }
       IProjectRepository ProjectRepo { get; }
       IPlateManufacturerRepository PlateManufacturerRepo { get; }
+      IRoleRepository RoleRepo { get; }
+      IUserRepository UserRepo { get; }
+      IPermissionRepository PermissionRepo { get; }
 
       ICategoryRepository СategoryRepo { get; }
 
