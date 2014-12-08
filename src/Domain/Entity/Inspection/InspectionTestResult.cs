4da60e16b9ffc83bd2788ccdc9a499802502e324
AleksMorozova <aleks_morozova91@mail.ru>
2014-12-08 18:42:11 +0200

    add inspection classes

---
+++ src/Domain/Entity/Inspection/InspectionTestResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.Inspection
+{
+    public class InspectionTestResult : TestResult<InspectionTestResult>
+    {
+        public InspectionTestResult()
+        {
+        }
+
+        public virtual PipelinePiece ControlEntity { get; set; }
+        public virtual string Type { get; set; }
+    }
+}
