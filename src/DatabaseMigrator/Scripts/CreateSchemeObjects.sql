0cc2e8194ad1b4db02f0f934d5f2fa5d56b25e19
AleksMorozova <aleks_morozova91@mail.ru>
2014-12-09 15:23:23 +0200

    add JointOperation

--- src/DatabaseMigrator/Scripts/CreateSchemeObjects.sql
+++ src/DatabaseMigrator/Scripts/CreateSchemeObjects.sql
@@ -169,6 +169,28 @@ CREATE TABLE [dbo].[PipeTest](
 ) ON [PRIMARY]
 
 SET ANSI_PADDING OFF
+/****** Object:  Table [dbo].[JointOperation]    Script Date: 11/4/2014 4:35:49 PM ******/
+SET ANSI_NULLS ON
+SET QUOTED_IDENTIFIER ON
+SET ANSI_PADDING ON
+CREATE TABLE [dbo].[JointOperation](
+	[id] [uniqueidentifier] NOT NULL,
+	[name] [nvarchar](50) NULL,
+	[isRequired] [bit] NULL,
+	[isTest] [bit] NULL,
+	[testHasAccepted] [bit] NULL,
+	[testHasToRepair] [bit] NULL,
+	[testHasToWithdraw] [bit] NULL,
+	[testResultRequired] [bit] NULL,
+	[isActive] [bit] NULL,
+ CONSTRAINT [PK_JointOperation] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+
+SET ANSI_PADDING OFF
 /****** Object:  Table [dbo].[PipeTestResult]    Script Date: 11/4/2014 4:35:49 PM ******/
 SET ANSI_NULLS ON
 SET QUOTED_IDENTIFIER ON
---
+++ src/Domain/Entity/Setup/JointOperation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity.Setup
+{
+    public class JointOperation : Item
+    {
+        public virtual string Name { get; set; }
+        public virtual bool IsRequired { get; set; }
+        public virtual bool IsTest { get; set; }
+        public virtual bool TestHasAcepted { get;  set; }
+        public virtual bool TestHasToRepair { get; set; }
+        public virtual bool TestHasToWithdrow { get; set; }
+        public virtual bool TestResultRequired { get; set; }
+        //TODO:
+        //public virtual ICollection<JointActionResults> { get; set; }
+        //public virtual ICollection<JointTestResults> { get; set; } 
+
+        public JointOperation()
+        {
+            //TODO: 
+            //this.JointActionResults = new List<JointActionResults>();
+            //this.JointTestResults = new List<JointTestResults>();
+        }
+
+        public JointOperation(string name, bool isRequired, bool isTest)
+        {
+            this.Name = name;
+            this.IsRequired = isRequired;
+            this.IsTest = isTest;
+            //TODO:
+            //this.JointActionResults = new List<JointActionResults>();
+            //this.JointTestResults = new List<JointTestResults>();
+        }
+
+
+    }
+}
