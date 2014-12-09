d82c4fbf134c171b049bcf0d38f7bd22ddb87a2c
DmitrySidorov <sidorov@email.ua>
2014-12-09 19:06:15 +0200

    add tables and entity

---
+++ src/Data/DAL/Mapping/ConnectorMap.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entity.Construction;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DAL.Mapping
+{
+    public class ConnectorMap: ClassMap<Connector>
+    {
+        public ConnectorMap()
+        {
+            Map(_ => _.Diameter).Column("diameter");
+            Map(_ => _.WallThickness).Column("wallThickness");
+            Map(_ => _.IsUsed).Column("isUsed");
+
+            References<Component>(x => x.Component).Column("componentId");
+        }
+    }
+}
---
+++ src/Data/DAL/Mapping/SpoolMap.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entity.Construction;
+using Domain.Entity.Mill;
+using FluentNHibernate.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.DAL.Mapping
+{
+    public class SpoolMap: ClassMap<Spool>
+    {
+        public SpoolMap()
+        {
+            Map(_ => _.PipeNumber).Column("pipeNumber");
+
+            References<Pipe>(x => x.Pipe).Column("pipeId");
+        }
+    }
+}
