c0343b477b93618326150397c311e10bf93ebf25
demmy <demiurge.ds@gmail.com>
2014-12-10 13:40:33 +0200

    Part entity & Transformer

---
+++ src/PrizmMainProject/Forms/InspectionParts/Search/Part.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrizmMain.Forms.InspectionParts.Search
+{
+    public class Part
+    {
+        public Guid Id;
+        public string Number;
+        public string Type;
+    }
+}
---
+++ src/PrizmMainProject/Forms/InspectionParts/Search/PartQuery.cs
@@ -0,0 +1,38 @@
+﻿using NHibernate.Transform;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrizmMain.Forms.InspectionParts.Search
+{
+    public class PartQuery : IResultTransformer
+    {
+        public static readonly string Sql = "";
+        public static readonly PartQuery Transformer = new PartQuery();
+
+        private PartQuery()
+        { }
+
+        #region IResultTransformer Members
+
+        public IList TransformList(IList collection)
+        {
+            return collection;
+        }
+
+        public object TransformTuple(object[] tuple, string[] aliases)
+        {
+            return new Part
+            {
+                Id = (Guid)tuple[0],
+                Number = (string)tuple[0],
+                Type = (string)tuple[1]
+            };
+        }
+
+        #endregion
+    }
+}
