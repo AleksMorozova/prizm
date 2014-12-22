224cbf68604cc05a79dc223af62ab14bcebd4c8a
demmy <demiurge.ds@gmail.com>
2014-12-22 16:32:51 +0200

    revert Excess whitespace after date in status notify #696

--- src/PrizmMainProject/Forms/MainChildForm/PrizmApplicationXtraForm.cs
+++ src/PrizmMainProject/Forms/MainChildForm/PrizmApplicationXtraForm.cs
@@ -420,7 +420,7 @@ namespace Prizm.Main.Forms.MainChildForm
         private void StatusNotifyText(string s)
         {
             var main = Program.MainForm as PrizmApplicationXtraForm;
-            var str = string.Format("[{0}] - {1}", DateTime.Now.ToShortTimeString().Trim(), s);
+            var str = string.Format("[{0}] - {1}", DateTime.Now.ToShortTimeString(), s);
             main.UpdateStatusBar(str);
         }
         #endregion
