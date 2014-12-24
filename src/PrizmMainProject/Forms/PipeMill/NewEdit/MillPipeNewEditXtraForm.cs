78b4adc827238f35292761f1c756924e8b4a336f
kapcia <kapcentrino@gmail.com>
2014-12-24 10:41:28 +0200

    Fix of issue #720

--- src/PrizmMainProject/Forms/PipeMill/NewEdit/MillPipeNewEditXtraForm.cs
+++ src/PrizmMainProject/Forms/PipeMill/NewEdit/MillPipeNewEditXtraForm.cs
@@ -286,7 +286,10 @@ namespace Prizm.Main.Forms.PipeMill.NewEdit
                 e.DisplayText = string.Empty;
 
             IList<Welder> welders = e.Value as IList<Welder>;
-            e.DisplayText = viewModel.FormatWeldersList(welders);
+            if (viewModel != null)
+            {
+                e.DisplayText = viewModel.FormatWeldersList(welders);
+            }
         }
 
         private void repositoryItemPopupWelders_QueryPopUp(object sender, CancelEventArgs e)
@@ -387,7 +390,10 @@ namespace Prizm.Main.Forms.PipeMill.NewEdit
                 e.DisplayText = string.Empty;
 
             IList<Inspector> inspectors = e.Value as IList<Inspector>;
-            e.DisplayText = viewModel.FormatInspectorList(inspectors);
+            if (viewModel != null)
+            {
+                e.DisplayText = viewModel.FormatInspectorList(inspectors);
+            }
         }
 
         /// <summary>
