fac019e42fc47907791d8b67eb1499ec815ffabd
demmy <demiurge.ds@gmail.com>
2014-12-24 17:08:43 +0200

    crash fix

--- src/PrizmMainProject/Forms/Settings/SettingsXtraForm.cs
+++ src/PrizmMainProject/Forms/Settings/SettingsXtraForm.cs
@@ -90,6 +90,7 @@ namespace Prizm.Main.Forms.Settings
             pipeNumberMaskRulesLabel.Text = Resources.Mask_Label;
             viewModel = (SettingsViewModel)Program.Kernel.GetService(typeof(SettingsViewModel));
             viewModel.ModifiableView = this;
+            viewModel.validatableView = this;
             viewModel.PropertyChanged += (s, eve) => IsModified = true;
 
             viewModel.LoadData();
