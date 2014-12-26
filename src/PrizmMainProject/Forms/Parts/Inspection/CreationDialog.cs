8639153e4f840d82f810e6244b31d196e45eaea6
olga-jane <olga.jane.vakhromova@gmail.com>
2014-12-26 11:38:01 +0200

    empty NumbersDialog. CreationDialog

---
+++ src/PrizmMainProject/Forms/Parts/Inspection/CreationDialog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Prizm.Main.Forms.Parts.Inspection
+{
+    public partial class CreationDialog : DevExpress.XtraEditors.XtraForm
+    {
+        public CreationDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
