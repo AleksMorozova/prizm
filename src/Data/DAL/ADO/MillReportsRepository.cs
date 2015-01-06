d6a26ce1f29d94e240331e3b1b7963b51b3b063e
DmitrySidorov <sidorov@email.ua>
2015-01-06 13:38:08 +0200

    fix bug: GetPipelineElements() method overloading

--- src/Data/DAL/ADO/MillReportsRepository.cs
+++ src/Data/DAL/ADO/MillReportsRepository.cs
@@ -231,7 +231,7 @@ namespace Prizm.Data.DAL.ADO
 
         public DataTable GetPipelineElements()
         { 
-            return GetPipelineElements(SQLQueryString.GetPipelinePieces);
+            return GetPipelineElements(SQLProvider.GetQuery(SQLProvider.SQLStatic.GetPipelinePieces).ToString());
         }
 
         public DataTable GetPipelineElements(string sqlQueryString)
@@ -247,7 +247,7 @@ namespace Prizm.Data.DAL.ADO
                     {
                         connection.Open();
                         command.Connection = connection;
-                        command.CommandText = SQLProvider.GetQuery(SQLProvider.SQLStatic.GetPipelinePieces).ToString();
+                        command.CommandText = sqlQueryString;
                         adapter.SelectCommand = command;
                         adapter.Fill(resultsTable);
                     }
