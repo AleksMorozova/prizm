9e805d74e6c01bb2bbbb9c5ec1de1acdfa607243
Alexander Dembinsky <alexander.dembinsky@gmail.com>
2015-01-12 15:16:05 +0200

    #653 Added serializable object for Pipe-related data.

---
+++ src/PrizmMainProject/Synch/SerializableEntities/PurchaseOrderObject.cs
@@ -0,0 +1,45 @@
+﻿using Prizm.Domain.Entity.Mill;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Prizm.Main.Synch.SerializableEntities
+{
+   [XmlType("PurchaseOrder")]
+   public class PurchaseOrderObject
+   {
+      public PurchaseOrderObject()
+      {
+      }
+
+      public PurchaseOrderObject(PurchaseOrder order)
+      {
+         this.Id = order.Id;
+         this.IsActive = order.IsActive;
+         this.Number = order.Number;
+         this.Date = order.Date;
+      }
+
+      public static implicit operator PurchaseOrderObject(PurchaseOrder order)
+      {
+         return order != null ? new PurchaseOrderObject(order) : null;
+      }
+
+      [XmlAttribute("Id")]
+      public Guid Id { get; set; }
+
+      [XmlAttribute("IsActive")]
+      public bool IsActive { get; set; }
+
+      [XmlAttribute("Number")]
+      public string Number { get; set; }
+
+      [XmlAttribute("Date")]
+      public DateTime? Date { get; set; }
+
+
+   }
+}
---
+++ src/PrizmMainProject/Synch/SerializableEntities/SpoolObject.cs
@@ -0,0 +1,59 @@
+﻿using Prizm.Domain.Entity.Construction;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Prizm.Main.Synch.SerializableEntities
+{
+   [XmlType("Spool")]
+   public class SpoolObject
+   {
+      public SpoolObject()
+      {
+      }
+
+      public SpoolObject(Spool spool)
+      {
+         this.Id = spool.Id;
+         this.IsActive = spool.IsActive;
+         this.PipeNumber = spool.PipeNumber;
+         this.Number = spool.Number;
+         this.Length = spool.Length;
+         this.IsAvailableToJoint = spool.IsAvailableToJoint;
+         this.ConstructionStatus = spool.ConstructionStatus;
+         this.InspectionStatus = spool.InspectionStatus;
+      }
+
+      public static implicit operator SpoolObject(Spool spool)
+      {
+         return spool != null ? new SpoolObject(spool) : null;
+      }
+
+      [XmlAttribute("Id")]
+      public Guid Id { get; set; }
+
+      [XmlAttribute("IsActive")]
+      public bool IsActive { get; set; }
+
+      [XmlAttribute("PipeNumber")]
+      public string PipeNumber { get; set; }
+
+      [XmlAttribute("Number")]
+      public string Number { get; set; }
+
+      [XmlAttribute("Length")]
+      public int Length { get; set; }
+
+      [XmlAttribute("IsAvailableToJoint")]
+      public bool IsAvailableToJoint { get; set; }
+
+      [XmlAttribute("ConstructionStatus")]
+      public PartConstructionStatus ConstructionStatus { get; set; }
+
+      [XmlAttribute("InspectionStatus")]
+      public PartInspectionStatus InspectionStatus { get; set; }
+   }
+}
