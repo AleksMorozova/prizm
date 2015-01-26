f7101cb658c30d990900102f7d7148a6b5fa9360
kapcia <kapcentrino@gmail.com>
2015-01-26 19:12:12 +0200

    Import and Export forms

--- src/PrizmMainProject/Forms/Synch/ExportForm.cs
+++ src/PrizmMainProject/Forms/Synch/ExportForm.cs
@@ -42,18 +42,15 @@ namespace Prizm.Main.Forms.Synch
       {
           return new List<LocalizedItem>()
           {
-              // layout items
-              //new LocalizedItem(pipeNumberLayout, "NewEditPipe_PipeNumberLabel"),
-
               // controls
-              //new LocalizedItem(attachmentsButton, "NewEditPipe_AttachmentsButton"),
-
+              new LocalizedItem(btnExport, "Export_ExportButton"),
+              new LocalizedItem(progressPanel,"Export_PleaseWaitPanel", "Export_ExportingData"),
+              new LocalizedItem(lblLog,"Export_LogLabel"),
+              new LocalizedItem(btnReexport, "Export_ReexportButton"),
+              
               // grid column headers
-              //new LocalizedItem(weldersGridColumn, "NewEditPipe_WeldersColumnHeader"),
-
-              // layout control groups
-              //new LocalizedItem(plateLayoutControlGroup, "NewEditPipe_PlateGroup"),
-
+              new LocalizedItem(portionId, "Export_PortionIdColumnHeader"),
+              new LocalizedItem(gridColumnExportDate, "Export_ExportDateolumnHeader"),
               // other
           };
       }
