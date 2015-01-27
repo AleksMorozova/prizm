4a0ea50e5ef4254d303a81dd1fbc3fe539e4e1da
demmy <demiurge.ds@gmail.com>
2015-01-27 18:21:00 +0200

    MillReportsXtraForm localization

--- src/PrizmMainProject/Forms/Reports/Mill/MillReportsXtraForm.cs
+++ src/PrizmMainProject/Forms/Reports/Mill/MillReportsXtraForm.cs
@@ -20,11 +20,11 @@ using Prizm.Main.Languages;
 
 namespace Prizm.Main.Forms.Reports.Mill
 {
-    [System.ComponentModel.DesignerCategory("Form")] 
+    [System.ComponentModel.DesignerCategory("Form")]
     public partial class MillReportsXtraForm : ChildForm
     {
         private MillReportsViewModel viewModel;
-        private ICommandManager commandManager = new CommandManager(); 
+        private ICommandManager commandManager = new CommandManager();
 
         public MillReportsXtraForm()
         {
@@ -43,7 +43,7 @@ namespace Prizm.Main.Forms.Reports.Mill
             statuses.DataSource = viewModel.Statuses;
             statuses.DisplayMember = "Text";
             statuses.ValueMember = "Name";
-            foreach (var item in EnumWrapper<MillReportType>.EnumerateItems())
+            foreach(var item in EnumWrapper<MillReportType>.EnumerateItems())
             {
                 reportTypes.Properties.Items.Add(new RadioGroupItem(item.Item1, item.Item2));
             }
@@ -74,18 +74,20 @@ namespace Prizm.Main.Forms.Reports.Mill
             return new List<LocalizedItem>()
             {
                 // layout items
-                //new LocalizedItem(pipeNumberLayout, "NewEditPipe_PipeNumberLabel"),
+                new LocalizedItem(reportTypesLayout, "MillReport_ReportTypesLabel"),
+                
+                new LocalizedItem(reportPeriodLabel, "MillReport_ReportPeriodLabel"),
+                new LocalizedItem(startDateLayout, "MillReport_StartDateLabel"),
+                new LocalizedItem(finalDateLayout, "MillReport_EndDateLabel"),
 
-                // controls
-                //new LocalizedItem(attachmentsButton, "NewEditPipe_AttachmentsButton"),
+                new LocalizedItem(testCategoriesLayout, "MillReport_CategoriesLabel"),
+                new LocalizedItem(statusesLayout, "MillReport_StatusesLabel"),
 
-                // grid column headers
-                //new LocalizedItem(weldersGridColumn, "NewEditPipe_WeldersColumnHeader"),
+                new LocalizedItem(createReportaLyoutGroup, "MillReport_CreateGroup"),
+                new LocalizedItem(previewLayoutGroup, "MillReport_PreviewGroup"),
 
-                // layout control groups
-                //new LocalizedItem(plateLayoutControlGroup, "NewEditPipe_PlateGroup"),
-
-                // other
+                new LocalizedItem(createReportButton, "MillReport_CreateButton"),
+                new LocalizedItem(previewButton, "MillReport_PreviewButton")
             };
         }
 
@@ -94,10 +96,10 @@ namespace Prizm.Main.Forms.Reports.Mill
         private void generalReportTypes_ItemCheck(object sender, DevExpress.XtraEditors.Controls.ItemCheckEventArgs e)
         {
             List<Guid> checkedItems = new List<Guid>();
-            foreach (var item in testCategories.CheckedItems)
+            foreach(var item in testCategories.CheckedItems)
             {
                 var category = item as Category;
-                if (category != null)
+                if(category != null)
                     checkedItems.Add(category.Id);
             }
             viewModel.SearchIds = checkedItems;
@@ -110,23 +112,22 @@ namespace Prizm.Main.Forms.Reports.Mill
             testCategories.Enabled = true;
             statuses.Enabled = true;
 
-            
-                if (selected != MillReportType.ByCategories)
-                {
-                    testCategories.Enabled = false;
-                    statuses.Enabled = false;
-                }
-        
+            if(selected != MillReportType.ByCategories)
+            {
+                testCategories.Enabled = false;
+                statuses.Enabled = false;
+            }
+
 
         }
 
         private void statuses_ItemCheck(object sender, ItemCheckEventArgs e)
         {
             List<string> statusList = new List<string>();
-            foreach (var item in statuses.CheckedItems)
+            foreach(var item in statuses.CheckedItems)
             {
                 var status = item as EnumWrapper<PipeTestResultStatus>;
-                if (status != null)
+                if(status != null)
                     statusList.Add(status.Value.ToString());
             }
             viewModel.SearchStatuses = statusList;
