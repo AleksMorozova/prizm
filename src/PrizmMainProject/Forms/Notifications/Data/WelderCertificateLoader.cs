047e28f3348b97e20e735ed0c3b8d605bc95f059
Andriy <uuauu@ua.fm>
2015-01-28 17:42:51 +0200

    fix crash at start (notifications) #1067

--- src/PrizmMainProject/Forms/Notifications/Data/WelderCertificateLoader.cs
+++ src/PrizmMainProject/Forms/Notifications/Data/WelderCertificateLoader.cs
@@ -20,18 +20,9 @@ namespace Prizm.Main.Forms.Notifications.Data
         {
             StringBuilder sb = new StringBuilder();
             sb.Append(
-                @" SELECT 
-                       [id]
-                      ,[firstName]
-                      ,[lastName]
-                      ,[middleName]
-                      ,[certificate]
-                      ,[certificateExpiration]
-                      ,[stamp]
-                      ,[grade]
-                      ,[isActive]
-                  FROM [prizm].[dbo].[Welder] 
-                  Where (DATEDIFF(day, GETDATE(), [certificateExpiration] ) < 5)");
+                @" SELECT  id, firstName, lastName, isActive, certificateExpiration
+                       FROM  Welder
+                WHERE   (DATEDIFF(day, GETDATE(), certificateExpiration) < 5) AND (isActive = 1) ");
             return sb.ToString();
         }
 
