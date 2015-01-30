a15a9e4d7984d1b0d262e19492ab40ddede2c9e3
kapcia <kapcentrino@gmail.com>
2015-01-30 10:46:54 +0200

    Audit form
    Issue #1171

--- src/PrizmMainProject/Languages/StringResources.cs
+++ src/PrizmMainProject/Languages/StringResources.cs
@@ -21,7 +21,72 @@ namespace Prizm.Main.Languages
 
         public static StringResource Audit_ElementNumberLabel = new StringResource {
             Id = "Audit_ElementNumberLabel",
-            Description = "Аудит. Надпись номера элемента."
+            Description = "Аудит. Надпись номера элемента."};
+        
+        public static StringResource Audit_PeriodLabel = new StringResource { 
+            Id = "Audit_PeriodLabel", 
+            Description = "Аудит. Надпись периода"};
+
+        public static StringResource Audit_StartDateLabel = new StringResource { 
+            Id = "Audit_StartDateLabel", 
+            Description = "Аудит. Надпись начальной даты)"};
+
+        public static StringResource Audit_EndDateLabel = new StringResource { 
+            Id = "Audit_EndDateLabel", 
+            Description = "Аудит. Надпись конечной даты)"};
+
+        public static StringResource Audit_UserLabel = new StringResource { 
+            Id = "Audit_UserLabel", 
+            Description = "Аудит. Надпись пользователя"};
+
+        public static StringResource Audit_SearchButton = new StringResource { 
+            Id = "Audit_SearchButton", 
+            Description = "Аудит. Надпись кнопки пользователя"};
+
+        public static StringResource Audit_UserColumnHeader = new StringResource { 
+            Id = "Audit_UserColumnHeader", 
+            Description = "Аудит. Надпись колонки пользователя"};
+
+        public static StringResource Audit_DateColumnHeader = new StringResource { 
+            Id = "Audit_DateColumnHeader", 
+            Description = "Аудит. Надпись колонки даты"};
+
+        public static StringResource Audit_EntityColumnHeader = new StringResource { 
+            Id = "Audit_EntityColumnHeader", 
+            Description = "Аудит. Надпись колонки изменяемой сущности"};
+
+        public static StringResource Audit_OldValueColumnHeader = new StringResource { 
+            Id = "Audit_OldValueColumnHeader", 
+            Description = "Аудит. Надпись колонки старого значения"};
+
+        public static StringResource Audit_NewValueColumnHeader = new StringResource { 
+            Id = "Audit_NewValueColumnHeader",
+            Description = "Аудит. Надпись колонки нового значения" };
+
+        public static StringResource Audit_FieldColumnHeader = new StringResource { 
+            Id = "Audit_FieldColumnHeader", 
+            Description = "Аудит. Надпись колонки поля"};
+
+        public static StringResource Audit_NumberColumnHeader = new StringResource { 
+            Id = "Audit_NumberColumnHeader", 
+            Description = "Аудит. Надпись колонки номера"};
+
+        public static StringResource Audit_SearchParametersGroup = new StringResource { 
+            Id = "Audit_SearchParametersGroup", 
+            Description = "Аудит. Надпись панели параметров поиска"};
+
+        public static StringResource Audit_RadioPeriod = new StringResource { 
+            Id = "Audit_RadioPeriod", 
+            Description = "Аудит. Надпись радиокнопки периода"};
+
+        public static StringResource Audit_RadioUser = new StringResource { 
+            Id = "Audit_RadioUser", 
+            Description = "Аудит. Надпись радиокнопки пользователя"};
+
+        public static StringResource Audit_TitleLabel = new StringResource
+        {
+            Id = "Audit_TitleLabel",
+            Description = "Текст в окне \"Аудит \""
         };
 
         #endregion //--- Audit --
