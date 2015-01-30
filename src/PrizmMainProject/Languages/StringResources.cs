893022d2abb2027d223e997ba52a2a8b91f1622c
AleksMorozova <aleks_morozova91@mail.ru>
2015-01-30 12:13:15 +0200

    localization

--- src/PrizmMainProject/Languages/StringResources.cs
+++ src/PrizmMainProject/Languages/StringResources.cs
@@ -17,6 +17,303 @@ namespace Prizm.Main.Languages
 
         #endregion // --- Common
 
+        #region PurchaseOrderForm
+        public static StringResource PurchaseOrder_NumberLabel = new StringResource
+        {
+            Id = "PurchaseOrder_NumberLabel",
+            Description = "Наряд-заказ. Надпись над полем номера"
+        };
+
+        public static StringResource PurchaseOrder_DateLabel = new StringResource
+        {
+            Id = "PurchaseOrder_DateLabel",
+            Description = "Наряд-заказ. Надпись над полем даты"
+        };
+        public static StringResource PurchaseOrder_SaveButton = new StringResource
+        {
+            Id = "PurchaseOrder_SaveButton",
+            Description = "Наряд-заказ.Кнопка сохранения"
+        };
+        public static StringResource PurchaseOrder_CancelButton = new StringResource
+        {
+            Id = "PurchaseOrder_CancelButton",
+            Description = "Наряд-заказ.Кнопка отмены"
+        };
+        #endregion PurchaseOrderForm
+
+        #region PasswordChange
+        public static StringResource PassChange_OldPassLabel = new StringResource
+        {
+            Id = "PassChange_OldPassLabel",
+            Description = "Смена пароля. Ввод старого пароля"
+        };
+
+        public static StringResource PassChange_PassLabel = new StringResource
+        {
+            Id = "PassChange_PassLabel",
+            Description = "Смена пароля. Ввод нового пароля"
+        };
+
+        public static StringResource PassChange_ConfirmPassLabel = new StringResource
+        {
+            Id = "PassChange_ConfirmPassLabel",
+            Description = "Смена пароля. Подтверждение пароля"
+        };
+
+        public static StringResource PassChange_OKButton = new StringResource
+        {
+            Id = "PassChange_OKButton",
+            Description = "Смена пароля. Кнопка подтверждения"
+        };
+
+        public static StringResource PassChange_CancelButton = new StringResource
+        {
+            Id = "PassChange_CancelButton",
+            Description = "Смена пароля. Кнопка отмены"
+        };
+        #endregion PasswordChange
+
+        #region MillReport
+        public static StringResource MillReport_ReportTypesLabel = new StringResource
+        {
+            Id = "MillReport_ReportTypesLabel",
+            Description = "Отчеты на заводе. Выбор типа отчета"
+        };
+
+        public static StringResource MillReport_ReportPeriodLabel = new StringResource
+        {
+            Id = "MillReport_ReportPeriodLabel",
+            Description = "Отчеты на заводе. Период формирования отчета"
+        };
+        public static StringResource MillReport_StartDateLabel = new StringResource
+        {
+            Id = "MillReport_StartDateLabel",
+            Description = "Отчеты на заводе. Начальная дата"
+        };
+
+        public static StringResource MillReport_EndDateLabel = new StringResource
+        {
+            Id = "MillReport_EndDateLabel",
+            Description = "Отчеты на заводе. Конечная дата"
+        };
+
+        public static StringResource MillReport_CategoriesLabel = new StringResource
+        {
+            Id = "MillReport_CategoriesLabel",
+            Description = "Отчеты на заводе. Категория контрольной операции"
+        };
+
+        public static StringResource MillReport_StatusesLabel = new StringResource
+        {
+            Id = "MillReport_StatusesLabel",
+            Description = "Отчеты на заводе. Статус контрольной операции"
+        };
+        public static StringResource MillReport_CreateGroup = new StringResource
+        {
+            Id = "MillReport_CreateGroup",
+            Description = "Отчеты на заводе. Создание отчета"
+        };
+        public static StringResource MillReport_PreviewGroup = new StringResource
+        {
+            Id = "MillReport_PreviewGroup",
+            Description = "Отчеты на заводе. Просмотр отчета"
+        };
+
+        public static StringResource MillReport_CreateButton = new StringResource
+        {
+            Id = "MillReport_CreateButton",
+            Description = "Отчеты на заводе. Кнопка создания отчета"
+        };
+
+        public static StringResource MillReport_PreviewButton = new StringResource
+        {
+            Id = "MillReport_PreviewButton",
+            Description = "Отчеты на заводе. Кнопка предпросмотра отчета"
+        };
+        public static StringResource MillReport_TypeByCategories = new StringResource
+        {
+            Id = "MillReport_TypeByCategories",
+            Description = "Отчеты на заводе. Тип отчета - По категориям"
+        };
+        public static StringResource MillReport_TypeByShipped = new StringResource
+        {
+            Id = "MillReport_TypeByShipped",
+            Description = "Отчеты на заводе. Тип отчета - По Отгруженным"
+        };
+        public static StringResource MillReport_TypeByProduced = new StringResource
+        {
+            Id = "MillReport_TypeByProduced",
+            Description = "Отчеты на заводе. Тип отчета - ПО произведенным"
+        };
+        public static StringResource MillReport_TypeGeneral = new StringResource
+        {
+            Id = "MillReport_TypeGeneral",
+            Description = "Отчеты на заводе. Тип отчета - Общий отчет"
+        };
+        #endregion MillReport
+
+        #region IncomingInspectionReport
+        public static StringResource InspectionReport_DateLabel = new StringResource
+        {
+            Id = "InspectionReport_DateLabel",
+            Description = "Период для формирования отчета"
+        };
+
+        public static StringResource InspectionReport_StartDateLabel = new StringResource
+        {
+            Id = "InspectionReport_StartDateLabel",
+            Description = "Отчет на входном контроле. Начальная дата"
+        };
+
+        public static StringResource InspectionReport_EndDateLabel = new StringResource
+        {
+            Id = "InspectionReport_EndDateLabel",
+            Description = "Отчет на входном контроле. Крнечная дата"
+        };
+
+        public static StringResource InspectionReport_CreateGroup = new StringResource
+        {
+            Id = "InspectionReport_CreateGroup",
+            Description = "Отчет на входном контроле. Создание отчета"
+        };
+
+        public static StringResource InspectionReport_PreviewGroup = new StringResource
+        {
+            Id = "InspectionReport_PreviewGroup",
+            Description = "Отчет на входном контроле. Предпросмотр отчета"
+        };
+
+        public static StringResource InspectionReport_PreviewButton = new StringResource
+        {
+            Id = "InspectionReport_PreviewButton",
+            Description = "Отчет на входном контроле. Кнопка предпросмотра отчета"
+        };
+
+        public static StringResource InspectionReport_CreateButton = new StringResource
+        {
+            Id = "InspectionReport_CreateButton",
+            Description = "Отчет на входном контроле. Кнопка создания отчета"
+        };
+        #endregion IncomingInspectionReport
+
+        #region Spool
+        public static StringResource Spool_PipeNumberLabel = new StringResource
+        {
+            Id = "Spool_PipeNumberLabel",
+            Description = "Катушка. Номер трубы для нарезания на кутушки"
+        };
+
+        public static StringResource Spool_SpoolNumberLabel = new StringResource
+        {
+            Id = "Spool_SpoolNumberLabel",
+            Description = "Катушка. Номер катушки"
+        };
+
+        public static StringResource Spool_PipeLengthLabel = new StringResource
+        {
+            Id = "Spool_PipeLengthLabel",
+            Description = "Катушка. Длина трубы"
+        };
+
+        public static StringResource Spool_SpoolLengthLabel = new StringResource
+        {
+            Id = "Spool_SpoolLengthLabel",
+            Description = "Катушка. Длина катушки"
+        };
+
+        public static StringResource Spool_PipeSearchGroup = new StringResource
+        {
+            Id = "Spool_PipeSearchGroup",
+            Description = "Катушка. Поиск трубы"
+        };
+
+        public static StringResource Spool_SpoolGroup = new StringResource
+        {
+            Id = "Spool_SpoolGroup",
+            Description = "Катушка. Данные о катушке"
+        };
+
+        public static StringResource Spool_LenghtGroup = new StringResource
+        {
+            Id = "Spool_LenghtGroup",
+            Description = "Катушка. Длины трубы и катушки"
+        };
+
+        public static StringResource Spool_InspectionDateColumn = new StringResource
+        {
+            Id = "Spool_InspectionDateColumn",
+            Description = "Катушка. Дата входного контроля"
+        };
+
+        public static StringResource Spool_InspectionResultColumn = new StringResource
+        {
+            Id = "Spool_InspectionResultColumn",
+            Description = "Катушка. Результат входного контроля"
+        };
+
+        public static StringResource Spool_InspectionInspectorsColumn = new StringResource
+        {
+            Id = "Spool_InspectionInspectorsColumn",
+            Description = "Катушка. Инспектора"
+        };
+
+        public static StringResource Spool_InspectionReasonColumn = new StringResource
+        {
+            Id = "Spool_InspectionReasonColumn",
+            Description = "Катушка. Комментарии по результату входного контроля"
+        };
+
+        public static StringResource Spool_SearchButton = new StringResource
+        {
+            Id = "Spool_SearchButton",
+            Description = "Катушка. Кнопка поиска трубы"
+        };
+
+        public static StringResource Spool_AttachButton = new StringResource
+        {
+            Id = "Spool_AttachButton",
+            Description = "Катушка. Кнопка вложений"
+        };
+
+        public static StringResource Spool_SaveButton = new StringResource
+        {
+            Id = "Spool_SaveButton",
+            Description = "Катушка. Кнопка сохранения"
+        };
+
+        public static StringResource Spool_DeactivatedCheckBox = new StringResource
+        {
+            Id = "Spool_DeactivatedCheckBox",
+            Description = "Катушка. Деактивация катушки"
+        };
+        #endregion Spool
+
+        #region MissingPortion
+        public static StringResource MissingPortion_missingNumbers = new StringResource
+        {
+            Id = "MissingPortion_missingNumbers",
+            Description = "Недостающие порции. Номера порций"
+        };
+
+        public static StringResource MissingPortion_recomendation= new StringResource
+        {
+            Id = "MissingPortion_recomendation",
+            Description = "Недостающие порции. Рекомендации"
+        };
+
+        public static StringResource MissingPortion_acceptButton = new StringResource
+        {
+            Id = "MissingPortion_acceptButton",
+            Description = "Недостающие порции. Кнопка подтверждения импорта"
+        };
+
+        public static StringResource MissingPortion_cancelButton = new StringResource
+        {
+            Id = "MissingPortion_cancelButton",
+            Description = "Недостающие порции. Кнопка отмены импорта"
+        };
+        #endregion MissingPortion
+
         #region --- Audit ---
 
         public static StringResource Audit_ElementNumberLabel = new StringResource {
@@ -32,7 +329,16 @@ namespace Prizm.Main.Languages
             Id = "AboutForm_TitleLabel",
             Description = "Текст в окне \"О программе \""
         };
-
+        public static StringResource About_VersionLabel = new StringResource
+        {
+            Id = "About_VersionLabel",
+            Description = "Версия программы"
+        };
+        public static StringResource About_AcceptButton = new StringResource
+        {
+            Id = "About_AcceptButton",
+            Description = "Кнопка ОК"
+        };
         #endregion // --- About ---
 
     }
