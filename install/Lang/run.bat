74217143b2e8f53e4d8e28942674b6520b472cb6
demmy <demiurge.ds@gmail.com>
2015-02-02 12:04:46 +0200

    template logic

--- install/Lang/run.bat
+++ install/Lang/run.bat
@@ -1,6 +1,24 @@
-rem Md Resources
-rem Resgen Strings.fr-FR.txt Resources\Strings.fr-FR.resources
-rem Resgen Strings.ru-RU.txt Resources\Strings.ru-RU.resources
-rem Resgen Strings.zh-CN.txt Resources\Strings.zh-CN.resources
-rem Resgen Strings.en-AU.txt Resources\Strings.en-AU.resources
-.\Resources\Resgen %1.txt .\Resources\%1.resources
+ECHO OFF
+
+IF [%1]==[] (
+ECHO TEMPLATE NOT SET
+EXIT 1
+)
+
+SET "name=%1"
+SET "file=%name%.resources"
+SET date=%DATE:~10,4%%DATE:~4,2%%DATE:~7,2%
+
+echo RESULT = %file%
+
+IF EXIST %name%.txt (
+ IF EXIST .\Resources\%file% (
+ echo EXISTED FILE WILL BE BACKUPED
+ ren .\Resources\%file% %name%-%date%.bak
+ ) ELSE (
+ rem
+ )
+.\Resources\Resgen %name%.txt .\Resources\%name%.resources
+) ELSE (
+echo TEMPLATE NOT EXIST
+)
