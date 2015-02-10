0a37357f78292c3f0732a60dfec196cbe6517630
demmy <demiurge.ds@gmail.com>
2015-02-10 11:36:30 +0200

    add const & properties

--- src/PrizmMainProject/Common/DateEditExtension.cs
+++ src/PrizmMainProject/Common/DateEditExtension.cs
@@ -8,16 +8,22 @@ namespace Prizm.Main.Common
 {
     public static class DateEditExtension
     {
+        private const int MINYEAR = 1950;
+        private const int MAXYEAR = 2150;
+
+        public int MinYear { get { return MINYEAR; } }
+        public int MaxYear { get { return MAXYEAR; } }
+
         public static void SetLimits(this DevExpress.XtraEditors.DateEdit edit)
         {
-            edit.Properties.MinValue = new DateTime(1950, 1, 1);
-            edit.Properties.MaxValue = new DateTime(2150, 1, 1);
+            edit.Properties.MinValue = new DateTime(MINYEAR, 1, 1);
+            edit.Properties.MaxValue = new DateTime(MAXYEAR, 1, 1);
         }
 
         public static void SetLimits(this DevExpress.XtraEditors.Repository.RepositoryItemDateEdit edit)
         {
-            edit.MinValue = new DateTime(1950, 1, 1);
-            edit.MaxValue = new DateTime(2150, 1, 1);
+            edit.MinValue = new DateTime(MINYEAR, 1, 1);
+            edit.MaxValue = new DateTime(MAXYEAR, 1, 1);
         }
     }
 }
