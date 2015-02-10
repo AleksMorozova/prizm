9988c3a5d6b161cbd2819933db03956abfc4c920
demmy <demiurge.ds@gmail.com>
2015-02-10 09:00:12 +0200

    add helper

---
+++ src/PrizmMainProject/Common/DateEditExtension.cs
@@ -0,0 +1,18 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prizm.Main.Common
+{
+    public static class DateEditExtension
+    {
+        public void SetLimits(this DateEdit edit)
+        {
+            edit.Properties.MinValue = new DateTime(1950, 1, 1);
+            edit.Properties.MaxValue = new DateTime(2150, 1, 1);
+        }
+    }
+}
