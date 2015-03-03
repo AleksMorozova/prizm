7fb80589c65edf188bbacb033cc3338ad388c2e1
DmitrySidorov <sidorov@email.ua>
2015-03-03 10:53:46 +0200

    add migration file

---
+++ src/DatabaseMigrator/Migrations/M20150303_AddDateTimePortion.cs
@@ -0,0 +1,27 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prizm.DatabaseMigrator.Migrations
+{
+    [Migration(20150303)]
+    public class M20150303_AddDateTimePortion : Migration
+    {
+        public override void Up()
+        {
+            Alter.Table("Portion")
+                .AlterColumn("exportDateTime")
+                .AsDateTime();
+        }
+
+        public override void Down()
+        {
+            Alter.Column("exportDateTime")
+                .OnTable("Portion")
+                .AsDate();
+        }
+    }
+}
