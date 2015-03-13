a0ccc8ad06371a119c6d8287538db19119cae9a4
demmy <demiurge.ds@gmail.com>
2015-03-13 09:14:43 +0200

    add maping for simple release note entity

---
+++ src/Data/DAL/Mapping/SimplePipeMap.cs
@@ -0,0 +1,30 @@
+﻿using Prizm.Domain.Entity.Construction;
+using Prizm.Domain.Entity.Mill;
+using Prizm.Domain.Entity.Setup;
+using FluentNHibernate.Mapping;
+using NHibernate.Mapping.ByCode.Conformist;
+using Prizm.Domain.Entity;
+using System;
+using Prizm.Domain.Entity.SimpleReleaseNote;
+
+namespace Prizm.Data.DAL.Mapping
+{
+    public class SimplePipeMap : SubclassMap<SimplePipe>
+    {
+        public SimplePipeMap()
+        {
+            #region --- Map ---
+            Map(_ => _.ToExport).Column("ToExport");
+            #endregion
+
+            #region --- References ---
+            References<Railcar>(x => x.Railcar).Column("railcarId").Cascade.SaveUpdate();
+	        References<PipeMillSizeType>(x => x.Type).Column("typeId");
+            #endregion
+
+            #region --- HasMany ---
+            HasMany<File>(x => x.Attachments).KeyColumn("item").Inverse().LazyLoad();
+            #endregion
+        }
+    }
+}
---
+++ src/Data/DAL/Mapping/SimpleRailcarMap.cs
@@ -0,0 +1,20 @@
+﻿using Prizm.Domain.Entity.Mill;
+using FluentNHibernate.Mapping;
+using Prizm.Domain.Entity.SimpleReleaseNote;
+
+namespace Prizm.Data.DAL.Mapping
+{
+    public class SimpleRailcarMap : SubclassMap<SimpleRailcar>
+    {
+        public SimpleRailcarMap()
+        {
+            Table("Railcar");
+            Map(x => x.Number).Column("number");
+            Map(_ => _.Destination, "destination");
+            Map(_ => _.Certificate, "certificate");
+            Map(_ => _.IsShipped, "isShipped");
+            HasMany(_ => _.Pipes).KeyColumn("railcarId").Cascade.SaveUpdate();
+            References<SimpleNote>(x => x.ReleaseNote).Column("releaseNoteId").Cascade.SaveUpdate();
+        }
+    }
+}
