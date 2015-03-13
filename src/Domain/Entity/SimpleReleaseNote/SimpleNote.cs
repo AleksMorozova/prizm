49fc842b03ac0c524d8a26347af79a47cd420dd9
demmy <demiurge.ds@gmail.com>
2015-03-13 09:08:51 +0200

    add Simplify entity for ReleaseNote

---
+++ src/Domain/Entity/SimpleReleaseNote/SimpleNote.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prizm.Domain.Entity.SimpleReleaseNote
+{
+    public class SimpleNote: Item
+    {
+        public SimpleNote()
+        {
+            this.Railcars = new List<SimpleRailcar>();
+        }
+
+        public virtual string Number { get; set; }
+        public virtual DateTime Date { get; set; }
+        public virtual bool Shipped { get; set; }
+
+        public virtual IList<SimpleRailcar> Railcars { get; set; }
+
+        public virtual IList<File> Attachments { get; set; }
+
+    }
+}
---
+++ src/Domain/Entity/SimpleReleaseNote/SimplePipe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Prizm.Domain.Entity.Setup;
+using System.Collections.Generic;
+using Prizm.Domain.Entity.Construction;
+using Prizm.Domain.Entity.Mill;
+
+namespace Prizm.Domain.Entity.SimpleReleaseNote
+{
+    public class SimplePipe : Part
+    {
+        public virtual PipeMillSizeType Type { get; set; }
+
+        public virtual SimpleRailcar Railcar { get; set; }
+
+        public virtual PipeMillStatus Status { get; set; }
+
+        public virtual bool ToExport { get; set; }
+
+        public virtual IList<File> Attachments { get; set; }
+    }
+}
