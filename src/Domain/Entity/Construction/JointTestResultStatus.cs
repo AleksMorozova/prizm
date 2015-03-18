c53146c8a7922819ad610c9ad6efe266622344e8
AleksMorozova <aleks_morozova91@mail.ru>
2015-03-18 17:55:01 +0200

    add comments

--- src/Domain/Entity/Construction/JointTestResultStatus.cs
+++ src/Domain/Entity/Construction/JointTestResultStatus.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Prizm.Domain.Entity.Construction
 {
+    /// <summary>
+    /// All items in this list should correspond to appropriate statuses for search. 
+    /// Used for all combo boxes with activity search criteria.
+    /// Please do not add items that will not go to combo choices.
+    /// Items must be numbered from 0 and without gaps.
+    /// Items may be removed if necessary.
+    /// </summary>
     public enum JointTestResultStatus
     {
         Undefined = 0,
