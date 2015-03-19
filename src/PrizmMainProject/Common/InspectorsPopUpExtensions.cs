ad6241512dce8cdd7850a4cf99d96233019bca02
demmy <demiurge.ds@gmail.com>
2015-03-19 14:46:22 +0200

    add inspectorsPopupContainer extensions

---
+++ src/PrizmMainProject/Common/InspectorsPopUpExtensions.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraEditors.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prizm.Main.Common
+{
+    public static class InspectorsPopUpExtensions
+    {
+        public static void SetSize(this RepositoryItemPopupContainerEdit container)
+        {
+            container.PopupFormSize = new System.Drawing.Size(400, 250);
+        }
+    }
+}
