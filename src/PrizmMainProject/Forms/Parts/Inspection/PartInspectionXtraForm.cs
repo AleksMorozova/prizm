03a9fd377862658a0c52b6408a6ecfe3ca2fc276
kapcia <kapcentrino@gmail.com>
2015-03-19 11:56:04 +0200

    Incoming inspection dialog #PRISM-114 Fixed
    Fixed displaying validation dialog
    Also added missing translation
    Issue:
    #PRISM-114 при вводе результатов или выборе инспектора любое нажатие
    клавиатуру приводит к сообщению Error. Do you want to correct value?

--- src/PrizmMainProject/Forms/Parts/Inspection/PartInspectionXtraForm.cs
+++ src/PrizmMainProject/Forms/Parts/Inspection/PartInspectionXtraForm.cs
@@ -18,6 +18,7 @@ using DevExpress.XtraGrid.Views.Grid;
 using Prizm.Main.Languages;
 using Prizm.Main.Documents;
 using Prizm.Main.Common;
+using DevExpress.XtraGrid.Views.Base;
 
 namespace Prizm.Main.Forms.Parts.Inspection
 {
@@ -276,6 +277,11 @@ namespace Prizm.Main.Forms.Parts.Inspection
             }
 
             view.RefreshData();
+        }
+
+        private void HandleInvalidRowException(object sender, InvalidRowExceptionEventArgs e)
+        {
+            e.ExceptionMode = DevExpress.XtraEditors.Controls.ExceptionMode.NoAction;
         }
 
     }
