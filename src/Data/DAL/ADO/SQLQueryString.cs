57be9bc60ec38d96c1f08ffd19ee3983a83b9a9a
AleksMorozova <aleks_morozova91@mail.ru>
2015-03-23 11:06:44 +0200

    add last inspection result to Inspection report

--- src/Data/DAL/ADO/SQLQueryString.cs
+++ src/Data/DAL/ADO/SQLQueryString.cs
@@ -134,7 +134,8 @@ group by productionDate  ";
             
             ORDER BY number";
 
-        private const string GetAllPipesFromInspection = @"select DISTINCT Pipe.number as number,  PipeMillSizeType.type as type, Pipe.wallThickness as wallThickness, Pipe.length as length, Heat.number as Heat_number
+        private const string GetAllPipesFromInspection = @"select DISTINCT Pipe.number as number,  PipeMillSizeType.type as type, Pipe.wallThickness as wallThickness, 
+Pipe.length as length, Heat.number as Heat_number, InspectionTestResult.status as status
           from  InspectionTestResult InspectionTestResult
 
           INNER JOIN 
