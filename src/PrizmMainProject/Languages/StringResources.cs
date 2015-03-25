f4f6ee891239f0ac8fbfe700a0a09f9ae338ce2c
kapcia <kapcentrino@gmail.com>
2015-03-25 15:55:11 +0200

    Component diameter editing #PRISM-141 Fixed
    Added new column
    Added logic of disabling editors is connector is connected
    Issue:
    #PRISM-141 Component diameter editing

--- src/PrizmMainProject/Languages/StringResources.cs
+++ src/PrizmMainProject/Languages/StringResources.cs
@@ -2767,6 +2767,12 @@ namespace Prizm.Main.Languages
             Description = "Толщина стенки, мм"
         };
 
+        public static StringResource ComponentNewEdit_IsConnectedGridColumn = new StringResource
+        {
+            Id = "ComponentNewEdit_IsConnectedGridColumn",
+            Description = "Соединен"
+        };
+
         public static StringResource ComponentNewEdit_InspectionLayoutGroup = new StringResource
         {
             Id = "ComponentNewEdit_InspectionLayoutGroup",
