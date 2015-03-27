5c20c56973fb5be90951d0c1a8439f705cd13eaa
demmy <demiurge.ds@gmail.com>
2015-03-27 10:45:54 +0200

    add hotkey to russian version of accept button

--- src/PrizmMainProject/Forms/Settings/Inspections/MillInspectionXtraForm.Designer.cs
+++ src/PrizmMainProject/Forms/Settings/Inspections/MillInspectionXtraForm.Designer.cs
@@ -328,7 +328,7 @@
             this.saveButton.Size = new System.Drawing.Size(87, 22);
             this.saveButton.StyleController = this.mainLayoutControl;
             this.saveButton.TabIndex = 17;
-            this.saveButton.Text = "Принять";
+            this.saveButton.Text = "&Принять";
             // 
             // frequency
             // 
