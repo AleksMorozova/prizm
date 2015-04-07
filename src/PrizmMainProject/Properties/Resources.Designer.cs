cad1b2183048bb7e46eee3e1b90b010b17c32b1e
AleksMorozova <aleks_morozova91@mail.ru>
2015-04-07 16:37:02 +0300

    update message

--- src/PrizmMainProject/Properties/Resources.Designer.cs
+++ src/PrizmMainProject/Properties/Resources.Designer.cs
@@ -2994,7 +2994,7 @@ namespace Prizm.Main.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Общее количество / произведенно труб:.
+        ///   Looks up a localized string similar to Общее количество / произведено труб:.
         /// </summary>
         internal static string Notification_InformationForSelectiveOperation {
             get {
