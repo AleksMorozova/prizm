8db29312ff4dc79976f9786d2196e0183fac8f95
DmitrySidorov <sidorov@email.ua>
2015-04-10 19:01:13 +0300

    add missing file

---
+++ src/PrizmMainProject/Common/CustomViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿using DevExpress.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prizm.Main.Common
+{
+    public class CustomViewModelBase: ViewModelBase
+    {
+        protected new void RaisePropertyChanged(string propertyName)
+        {
+            if (!Program.IsCommandRunning)
+            {
+                base.RaisePropertyChanged(propertyName);
+            }
+        }
+    }
+}
