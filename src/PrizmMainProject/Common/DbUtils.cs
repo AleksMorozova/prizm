543c79831e517e1952ba2d9d61fceabd80d7f2d6
demmy <demiurge.ds@gmail.com>
2015-04-21 17:23:23 +0300

    optimize dblog by checkpoint&shrink

---
+++ src/PrizmMainProject/Common/DbUtils.cs
@@ -0,0 +1,32 @@
+﻿using NHibernate;
+using Prizm.Data.DAL.Hibernate;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Prizm.Main.Common
+{
+    public static class DbUtils
+    {
+        public static void LogOptimize()
+        {
+            
+
+            string connectionString = ConfigurationManager.ConnectionStrings["PrismDatabase"].ConnectionString;
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder(connectionString);
+            string database = builder.InitialCatalog;
+            string logName = database + "_log";
+
+            string query = string.Format(@"checkpoint; DBCC SHRINKFILE ({0});", logName);
+            using(var session = HibernateUtil.OpenSession(false))
+            {
+                session.CreateSQLQuery(query).ExecuteUpdate();
+            }
+        }
+    }
+}
